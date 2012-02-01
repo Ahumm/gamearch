c3cc8b00ad364afee46062e5ee7f1519e09992fc
Tate Larsen <larset2@gmail.com>
2012-02-01 16:38:26 -0500

    genericy

--- src/globaldefs.h
+++
@@ -1,6 +0,0 @@
-#ifndef _GLOBALDEFS_H_
-#define _GLOBALDEFS_H_
-
-#define PI 3.14159265
-
-#endif
--- src/includes.h
+++
@@ -1,8 +0,0 @@
-#ifndef _INCLUDES_H_
-#define _INCLUDES_H_
-
-#include "globaldefs.h"
-#include "vector.h"
-#include "matrix.h"
-
-#endif
